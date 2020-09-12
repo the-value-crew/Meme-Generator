951ccdcab19a541d1f3d094a13a4e1d950387974
Roshan <arobce@gmail.com>
2020-09-12 12:28:03 +0545

    Corrects tests api.test.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,15 @@
 const memeApiEndpoint = "https://api.imgflip.com/get_memes";
+const axios = require('axios');
 
 
-const getAllMemes =  async (axios) => {
-    const response = await axios.get(memeApiEndpoint).then((response)=>{
+const getAllMemes = async (_) => {
+    const response = await axios.get(memeApiEndpoint).then((response) => {
         const data = response.data;
         return {
-            memes: data.data.memes.splice(0,10)
+            memes: data.data.memes.splice(0, 10)
         }
-    }).catch(function(error){
-        return "Something went wrong: "+error;
+    }).catch(function (error) {
+        return "Something went wrong: " + error;
     });
     return response;
 }
--- src/utils/api.test.js
+++ src/utils/api.test.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 const api = require('./api');
 
 const memeApiEndpoint = "https://api.imgflip.com/get_memes";
-const mockMemeData = {
-    "memes": [
+const mockMemeData =
+    [
         {
             "id": "181913649",
             "name": "Drake Hotline Bling",
@@ -28,36 +28,37 @@ const mockMemeData = {
             "height": 908,
             "box_count": 2
         },
-    ]
-}
+    ];
 const mockSucessResponse = {
-    "success": true,
     "data": {
-        "memes": [
-            {
-                "id": "181913649",
-                "name": "Drake Hotline Bling",
-                "url": "https://i.imgflip.com/30b1gx.jpg",
-                "width": 1200,
-                "height": 1200,
-                "box_count": 2
-            },
-            {
-                "id": "112126428",
-                "name": "Distracted Boyfriend",
-                "url": "https://i.imgflip.com/1ur9b0.jpg",
-                "width": 1200,
-                "height": 800,
-                "box_count": 3
-            },
-            {
-                "id": "87743020",
-                "name": "Two Buttons",
-                "url": "https://i.imgflip.com/1g8my4.jpg",
-                "width": 600,
-                "height": 908,
-                "box_count": 2
-            }]
+        "success": true,
+        "data": {
+            "memes": [
+                {
+                    "id": "181913649",
+                    "name": "Drake Hotline Bling",
+                    "url": "https://i.imgflip.com/30b1gx.jpg",
+                    "width": 1200,
+                    "height": 1200,
+                    "box_count": 2
+                },
+                {
+                    "id": "112126428",
+                    "name": "Distracted Boyfriend",
+                    "url": "https://i.imgflip.com/1ur9b0.jpg",
+                    "width": 1200,
+                    "height": 800,
+                    "box_count": 3
+                },
+                {
+                    "id": "87743020",
+                    "name": "Two Buttons",
+                    "url": "https://i.imgflip.com/1g8my4.jpg",
+                    "width": 600,
+                    "height": 908,
+                    "box_count": 2
+                }]
+        }
     }
 }
 
@@ -65,17 +66,19 @@ jest.mock('axios');
 
 it("returns appropriate data from method", async () => {
 
-    axios.get.mockImplementationOnce(() => Promise.resolve(mockSucessResponse));
+    axios.get.mockResolvedValue(mockSucessResponse);
 
-    await api.getAllMemes(axios).then(({ memes }) => expect(memes).toEqual(mockMemeData));
+    await api.getAllMemes().then(({ memes }) => {
+        expect(memes).toEqual(mockMemeData)
+    });
     expect(axios.get).toHaveBeenCalledTimes(1);
     expect(axios.get).toHaveBeenCalledWith(memeApiEndpoint);
 });
 
-// it("returns appropriate error message from method", async () => {
-//     axios.get.mockRejectedValue(new Error('Connection Error'));
+it("returns appropriate error message from method", async () => {
+    axios.get.mockRejectedValue(new Error('Connection Error'));
 
-//     await api.getAllMemes(axios).then(data => expect(data).toEqual("Something went wrong: Error: Connection Error"));
-//     expect(axios.get).toHaveBeenCalledTimes(2);
-//     expect(axios.get).toHaveBeenCalledWith(memeApiEndpoint);
-// })
+    await api.getAllMemes(axios).then(data => expect(data).toEqual("Something went wrong: Error: Connection Error"));
+    expect(axios.get).toHaveBeenCalledTimes(2);
+    expect(axios.get).toHaveBeenCalledWith(memeApiEndpoint);
+})
