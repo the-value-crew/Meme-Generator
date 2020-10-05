a8ea4b7a880806a43aab430b2c110ad2bf11b455
Roshan <arobce@gmail.com>
2020-10-05 19:47:00 +0545

    Updates

---
+++ precache-manifest.88a833466d081379470df00a608fcc40.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccbe9e1cfc1896204a2f96ea7ec58b2",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "dadd404f7dbb45b80e5c",
+    "url": "/Meme-Generator/static/css/main.5e7e3142.chunk.css"
+  },
+  {
+    "revision": "1cd277cbef418c652e55",
+    "url": "/Meme-Generator/static/js/2.4a49f204.chunk.js"
+  },
+  {
+    "revision": "74549c08325e899dd17d787a1ff374cf",
+    "url": "/Meme-Generator/static/js/2.4a49f204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dadd404f7dbb45b80e5c",
+    "url": "/Meme-Generator/static/js/main.e6451b58.chunk.js"
+  },
+  {
+    "revision": "e72c705d5b0f5a6ba670",
+    "url": "/Meme-Generator/static/js/runtime-main.fa408ef4.js"
+  },
+  {
+    "revision": "35893e77e76e34d0e96ee515b48e58ac",
+    "url": "/Meme-Generator/static/media/Gilroy-Bold.35893e77.woff2"
+  },
+  {
+    "revision": "3d2077ee15e95de6519139b00ed0d0ac",
+    "url": "/Meme-Generator/static/media/Gilroy-Bold.3d2077ee.woff"
+  },
+  {
+    "revision": "cd092718f0a85e666b39d436fd3b6031",
+    "url": "/Meme-Generator/static/media/Gilroy-BoldItalic.cd092718.woff2"
+  },
+  {
+    "revision": "ef76fc8a3f842712714f69bd68950c01",
+    "url": "/Meme-Generator/static/media/Gilroy-BoldItalic.ef76fc8a.woff"
+  },
+  {
+    "revision": "319f239104ddc0f7d3dab638fdb87ddc",
+    "url": "/Meme-Generator/static/media/Gilroy-Light.319f2391.woff"
+  },
+  {
+    "revision": "77055150e89b69a50154d005b6f7d093",
+    "url": "/Meme-Generator/static/media/Gilroy-Light.77055150.woff2"
+  },
+  {
+    "revision": "aa3b18d16c8c6800d38701afea994461",
+    "url": "/Meme-Generator/static/media/Gilroy-Medium.aa3b18d1.woff"
+  },
+  {
+    "revision": "af1cc82e294e7d528f8ccd2dd5b47b8a",
+    "url": "/Meme-Generator/static/media/Gilroy-Medium.af1cc82e.woff2"
+  },
+  {
+    "revision": "9430034960b7471313a093a57f086d2b",
+    "url": "/Meme-Generator/static/media/Gilroy-Regular.94300349.woff2"
+  },
+  {
+    "revision": "a104a879c495fe3a27840fda87a803d2",
+    "url": "/Meme-Generator/static/media/Gilroy-Regular.a104a879.woff"
+  },
+  {
+    "revision": "044e719779318e4b6afc26490767bf14",
+    "url": "/Meme-Generator/static/media/Gilroy-SemiBold.044e7197.woff"
+  },
+  {
+    "revision": "5f6cd2ddd84d10241f045c37e605685d",
+    "url": "/Meme-Generator/static/media/Gilroy-SemiBold.5f6cd2dd.woff2"
+  },
+  {
+    "revision": "8db53187ecfba9fa2a36378fb6c84e06",
+    "url": "/Meme-Generator/static/media/cog.8db53187.svg"
+  },
+  {
+    "revision": "2b2138f3af2701eec3e6655191d37f4f",
+    "url": "/Meme-Generator/static/media/plus-circle.2b2138f3.svg"
+  },
+  {
+    "revision": "5ee04a9492cf7f15bf12969d19ca801f",
+    "url": "/Meme-Generator/static/media/undo-alt.5ee04a94.svg"
+  }
+]);
